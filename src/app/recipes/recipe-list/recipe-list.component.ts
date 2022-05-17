ef55e35d7a761641f6ad2f3f03ccfa1b0e71cc37
Harry <hdhillon478@gmail.com>
2022-05-17 08:40:59 +0100

    Cut recipe-list data into recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,19 +9,6 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'Fish and Chips',
-      'This is a description of Fish and Chips',
-      'https://www.thespruceeats.com/thmb/-Hq2aVGdlH43SB1R-A7UK2dCb6k=/1500x844/smart/filters:no_upscale()/best-fish-and-chips-recipe-434856-Hero-5b61b89346e0fb00500f2141.jpg'
-    ),
-    new Recipe(
-      'Aloo Parothe',
-      'This is description on Aloo Parothe',
-      'https://static.toiimg.com/thumb/53109843.cms?imgsize=244340&width=800&height=800'
-    ),
-  ];
-
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,1 +1,16 @@
-export class RecipeService {}
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Fish and Chips',
+      'This is a description of Fish and Chips',
+      'https://www.thespruceeats.com/thmb/-Hq2aVGdlH43SB1R-A7UK2dCb6k=/1500x844/smart/filters:no_upscale()/best-fish-and-chips-recipe-434856-Hero-5b61b89346e0fb00500f2141.jpg'
+    ),
+    new Recipe(
+      'Aloo Parothe',
+      'This is description on Aloo Parothe',
+      'https://static.toiimg.com/thumb/53109843.cms?imgsize=244340&width=800&height=800'
+    ),
+  ];
+}
