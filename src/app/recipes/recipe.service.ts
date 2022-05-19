7e0983b488b1a408b1a5c8169d34e453be4fe787
Harry <hdhillon478@gmail.com>
2022-05-19 10:34:43 +0100

    Update recipe model and service with ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -8,12 +9,14 @@ export class RecipeService {
     new Recipe(
       'Fish and Chips',
       'This is a description of Fish and Chips',
-      'https://www.thespruceeats.com/thmb/-Hq2aVGdlH43SB1R-A7UK2dCb6k=/1500x844/smart/filters:no_upscale()/best-fish-and-chips-recipe-434856-Hero-5b61b89346e0fb00500f2141.jpg'
+      'https://www.thespruceeats.com/thmb/-Hq2aVGdlH43SB1R-A7UK2dCb6k=/1500x844/smart/filters:no_upscale()/best-fish-and-chips-recipe-434856-Hero-5b61b89346e0fb00500f2141.jpg',
+      [new Ingredient('Fish', 1), new Ingredient('Potatoes', 5)]
     ),
     new Recipe(
       'Aloo Parothe',
       'This is description on Aloo Parothe',
-      'https://static.toiimg.com/thumb/53109843.cms?imgsize=244340&width=800&height=800'
+      'https://static.toiimg.com/thumb/53109843.cms?imgsize=244340&width=800&height=800',
+      [new Ingredient('Flour', 1), new Ingredient('Potatoes', 5)]
     ),
   ];
 
