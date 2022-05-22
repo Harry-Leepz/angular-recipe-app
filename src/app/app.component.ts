29d947635ea514516f8b69b2321027fc5f025384
Harry <hdhillon478@gmail.com>
2022-05-22 11:48:59 +0100

    Remove old logic to hide/show features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  loadedFeature = 'recipes';
-
-  // onNavigate listens for the emitted feature sent on the user click and
-  // sets the value of loadedFeature to the emitted arguement.
-  // *ngIf is used on the component selector to show or hide components
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
-}
+export class AppComponent {}
